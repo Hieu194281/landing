019710a4f020a750f8a4799b7bb48c442ef7686d
HieuNT <trunghieu220801@gmail.com>
2025-08-12 10:43:30 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       >
         {/* dark overlay */}
 
-        <div className="relative z-10 px-6 py-20 sm:py-28 lg:py-32 text-center text-white">
+        <div className="relative z-10 px-6 text-center text-white">
           <h1 className=" hero-title 
           text-7xl font-extrabold tracking-tight mb-6 text-[#FFD700]">
             ¡30% <span className="text-[#FFD700]">EXTRA!</span>
@@ -22,14 +22,19 @@ export default function Home() {
           </h2>
           <div className="my-8">
             <a
-              href="https://api.whatsapp.com/send/?phone=84961439036&text&type=phone_number&app_absent=0"
+              href="https://api.whatsapp.com/send/?phone=541158406428&text&type=phone_number&app_absent=0"
               target="_blank"
               rel="noopener noreferrer"
 
             >
-              <i className="fab fa-whatsapp text-[#25d366] text-[180px] inline-block transition-transform duration-300 ease-in-out hover:scale-110"></i>
+              <i className="fab fa-whatsapp text-[#1fa953] text-[180px] inline-block transition-transform duration-300 ease-in-out hover:scale-110"></i>
             </a>
+
           </div>
+          <h2 className="hero-title 
+          !text-2xl font-extrabold tracking-tight mb-6 !text-white">
+            +54 1158406428
+          </h2>
 
           <div className="flex gap-16 justify-center mt-16">
             <div className=" feature">
